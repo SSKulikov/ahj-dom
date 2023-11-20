192ee9437c08610c41a9d91ddb3d81e7e8739f9f
Sergey Kulikov <kylik11@mail.ru>
2023-11-20 12:33:09 +0300

    yes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
 import './css/style.css';
-
 import './js/app';
 
 // TODO: write your code in app.js
-function noLocalBranchConflict(branch) {
-    return expectOutputEmpty(
-      'git branch --list ' + branch,
-      'local branch with name already exists'
-    );
-  }
